3463589192b1a065a5de5a0f4d95395fcb7088b8
Daniel Galetti <danielgaletti@alunos.utfpr.edu.br>
2021-10-16 14:45:19 -0300

    Feat: Creating a new version to Campanha IMEL Adorarte

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="toolbar">
+                <div className="">
+                    <span> IMEL Adorarte</span>
+                </div>
+                <div className="">
+                    <button> Button 1 </button>
+                    <button> Button 1 </button>
+                    <button> Button 1 </button>
+                </div>
+            </div>
+        </header>
+    );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header.js'
+import './style.css'
+
+export default function Home(){
+    return(
+        <div>
+            <Header />
+            <main className="main"></main>
+        </div>
+    )
+}
